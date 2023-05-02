b85c4f5fa925fd9e09c17bfa43ed3962f8ef7ed1
Lisa Schwetlick <schwetlick.lisa@gmail.com>
2023-05-02 17:07:30 +0200

    basic structure added

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.get('/home', (req, res)=>{
+    res.sendFile(__dirname + '/views/home.html')
+});
+
+app.get('/about', (req, res)=>{
+    res.sendFile(__dirname + '/views/about.html')
+});
+
+app.get('/works', (req, res)=>{
+    res.sendFile(__dirname + '/views/about.html')
+});
+
+app.get('/gallery', (req, res)=>{
+    res.sendFile(__dirname + '/views/about.html')
+});
+
+app.get('*', (req, res)=>{
+    res.sendFile(__dirname + "/views/404.html");
+});
+
+console.log(__dirname)
+app.listen(3000);
